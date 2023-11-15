d2077b8d5a2829ebe5b51640fb57a6cd3dd90c43
LSeraphine <738232585@qq.com>
2023-11-15 22:31:42 +0800

    创建项目，创建多语言文件

---
+++ src/DataHarbor/ViewModels/Pages/DashboardViewModel.cs
@@ -0,0 +1,24 @@
+﻿// This Source Code Form is subject to the terms of the MIT License.
+// If a copy of the MIT was not distributed with this file, You can obtain one at https://opensource.org/licenses/MIT.
+// Copyright (C) Leszek Pomianowski and WPF UI Contributors.
+// All Rights Reserved.
+
+using DataHarbor.Resources;
+using System.Globalization;
+
+namespace DataHarbor.ViewModels.Pages
+{
+    public partial class DashboardViewModel : ObservableObject
+    {
+        [ObservableProperty]
+        private int _counter = 0;
+
+        [RelayCommand]
+        private void OnCounterIncrement()
+        {
+            Counter++;
+            //切换语言
+            ////ResourceService.Instance.ChangeLanguage(new CultureInfo("en-US"));
+        }
+    }
+}
---
+++ src/DataHarbor/ViewModels/Windows/MainWindowViewModel.cs
@@ -0,0 +1,56 @@
+﻿// This Source Code Form is subject to the terms of the MIT License.
+// If a copy of the MIT was not distributed with this file, You can obtain one at https://opensource.org/licenses/MIT.
+// Copyright (C) Leszek Pomianowski and WPF UI Contributors.
+// All Rights Reserved.
+
+using DataHarbor.Views.Pages;
+using System.Collections.ObjectModel;
+using System.Windows;
+using Wpf.Ui.Common;
+using Wpf.Ui.Controls;
+
+namespace DataHarbor.ViewModels.Windows
+{
+    public partial class MainWindowViewModel : ObservableObject
+    {
+        [ObservableProperty]
+        private string _applicationTitle = "WPF UI - DataHarbor";
+
+        [ObservableProperty]
+        private ObservableCollection<object> _menuItems = new()
+        {
+            new NavigationViewItem()
+            {
+                Content = "Home",
+                Icon = new SymbolIcon { Symbol = SymbolRegular.Home24 },
+                TargetPageType = typeof(Views.Pages.DashboardPage)
+            },
+            //多栏
+            new NavigationViewItem("Data", SymbolRegular.DataHistogram24, typeof(DashboardPage))
+            {
+                MenuItems = new object[]
+                {
+                    new NavigationViewItem(nameof(Snackbar), typeof(DataPage)),
+                    new NavigationViewItem(nameof(ContentDialog), typeof(DashboardPage)),
+                }   
+            },
+        };
+
+        [ObservableProperty]
+        private ObservableCollection<object> _footerMenuItems = new()
+        {
+            new NavigationViewItem()
+            {
+                Content = "Settings",
+                Icon = new SymbolIcon { Symbol = SymbolRegular.Settings24 },
+                TargetPageType = typeof(Views.Pages.SettingsPage)
+            }
+        };
+
+        [ObservableProperty]
+        private ObservableCollection<MenuItem> _trayMenuItems = new()
+        {
+            new MenuItem { Header = "Home", Tag = "tray_home" }
+        };
+    }
+}
